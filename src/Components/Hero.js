94e862c1986ec210a69a8fbb45786b4d8547d3bd
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-05 10:03:46 +0200

    adding heading and p element into hero component

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,6 +5,8 @@ export default function Hero() {
     return(
         <div className='hero-wrapper'>
             <img className='hero-image' src={heroImage} alt='collection of random images forming grid'/>
+            <h1>Online Experience</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts - all without leaving home</p>
         </div>
     )
 }
