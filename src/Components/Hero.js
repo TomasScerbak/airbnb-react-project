b8b2e52ae7e4e92f070171f0ed7bca812779b041
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-05 10:28:23 +0200

    wrapping hero heading and hero text into hero body

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,9 +4,11 @@ import heroImage from '../Images/hero-grid.png'
 export default function Hero() {
     return(
         <div className='hero-wrapper'>
-            <img className='hero-image' src={heroImage} alt='collection of random images forming grid'/>
-            <h1>Online Experience</h1>
-            <p>Join unique interactive activities led by one-of-a-kind hosts - all without leaving home</p>
+            <img className='hero__image' src={heroImage} alt='collection of random images forming grid'/>
+            <div class="hero__body">
+                <h1>Online Experience</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts - all without leaving home</p>
+            </div>
         </div>
     )
 }
