c8a5d1d3073235d041608eb3bbd0eaf22f0bc4aa
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-05 09:35:43 +0200

    creating hero component js file

---
+++ src/Components/Hero.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import heroImage from '../Images/hero-grid.png'
+
+export default function Hero() {
+    return(
+        <img src={heroImage} alt='collection of random images forming grid'/>
+    )
+}
