0247fe30dc33a8f5a814928a99638e962142cca0
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-06 16:30:11 +0200

    finalizing card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import cardImage from '../Images/cardimageone.png'
+import starImage from '../Images/Star.png'
 
 export default function Card() {
     return (
         <section>
             <div className='card'>
                 <img src={cardImage} alt="random person" />
+                <div class="rating">
+                    <img className='star' src={starImage} alt="star rating"/>
+                    <span>5.0</span>
+                    <span>(6).USA</span>
+                </div>
             </div>
         </section>
     )
