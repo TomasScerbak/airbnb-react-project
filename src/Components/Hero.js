50074e1e16d632ff358e887de9ee819b0ae9dd78
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-06 16:04:48 +0200

    remove bug from hero component and add proper className instead of class

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     return(
         <section className='hero-wrapper'>
             <img className='hero__image' src={heroImage} alt='collection of random images forming grid'/>
-            <div class="hero__body">
+            <div className="hero__body">
                 <h1>Online Experience</h1>
                 <p className='hero__text'>Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
             </div>
