793dc15b761b8b79a66c9937c70dd39b059f1ac8
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-06 16:14:01 +0200

    create card component js file

---
+++ src/Components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <section className='hero-wrapper'>
+            <div className='card'></div>
+        </section>
+    )
+}
