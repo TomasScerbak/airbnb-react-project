2f825ba4ab744c897ea9f9cedcf0cfe8163209c4
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-06 16:16:13 +0200

    created card div and import img

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import cardImage from '../Images/cardimageone.png'
 
 export default function Card() {
     return (
         <section className='hero-wrapper'>
-            <div className='card'></div>
+            <div className='card'>
+                <img src={cardImage} alt="random person" />
+            </div>
         </section>
     )
 }
