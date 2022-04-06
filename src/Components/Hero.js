536748f10c2df575337665fbd06f8a044584ac80
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-06 16:02:21 +0200

    replacing div with section element in hero component file

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,12 +3,12 @@ import heroImage from '../Images/hero-grid.png'
 
 export default function Hero() {
     return(
-        <div className='hero-wrapper'>
+        <section className='hero-wrapper'>
             <img className='hero__image' src={heroImage} alt='collection of random images forming grid'/>
             <div class="hero__body">
                 <h1>Online Experience</h1>
                 <p className='hero__text'>Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
             </div>
-        </div>
+        </section>
     )
 }
