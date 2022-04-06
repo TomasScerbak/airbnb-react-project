89607f5665d734589876f95d2f23193f0d2ac93b
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-06 16:20:46 +0200

    add styles for card component for mobile and wider screens

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import cardImage from '../Images/cardimageone.png'
 
 export default function Card() {
     return (
-        <section className='hero-wrapper'>
+        <section>
             <div className='card'>
                 <img src={cardImage} alt="random person" />
             </div>
