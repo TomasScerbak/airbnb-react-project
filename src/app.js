5b653401e1e1c13d510b39a4b658da8c2cebee62
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-07 15:05:26 +0200

    importing Contacts component into app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
-import Card from './Components/Card'
+import Contacts from './Components/Contact'
 
 export default function App() {
     return(
         <div className='container'>
             <Navbar />
             <Hero />
-            <Card />
+            <Contacts />
         </div>
     )
 }
