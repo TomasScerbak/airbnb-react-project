7195ac5c7602fa88fd3b65c9d2764f9cb7b0a268
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-07 15:41:36 +0200

    importing props to card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import cardImage from '../Images/cardimageone.png'
 import starImage from '../Images/Star.png'
 
-export default function Card() {
+export default function Card(props) {
     return (
                 <div className='card'>
-                    <img src={cardImage} alt="random person" />
+                    <img src={props.img} alt="random person" />
                     <div class="rating">
                         <img className='star' src={starImage} alt="star rating"/>
-                        <span className='opacity'>5.0</span>
-                        <span className='opacity'>(6).USA</span>
+                        <span className='opacity'>{props.rating}</span>
+                        <span className='opacity'>(6).{props.city}</span>
                     </div>
-                    <article className='article'>Life lessons with Kathia Zaferes</article>
-                    <span className='price'>From $136 </span>
+                    <article className='article'>{props.name}</article>
+                    <span className='price'>{props.price}</span>
                     <span>/ person</span>
                 </div>
     )
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,36 +1,37 @@
 import React from 'react'
 import Card from '../Components/Card'
+import cardImage from '../Images/cardimageone.png'
 
-export default function Contacts(props) {
+export default function Contacts() {
     return(
         <div className='contacts'>
             <Card
-            img='../Images/cardimageone.png' 
-            data='Zidan'
+            img={cardImage}
+            city='USA'
             rating='5'
-            name='Pizda'
-            price='158$'
+            name='Alda'
+            price='157$'
             />
             <Card
-            img='../Images/cardimageone.png' 
-            data='Zidan'
-            rating='5'
-            name='Pizda'
-            price='158$'
+            img={cardImage}
+            city='Chile'
+            rating='4'
+            name='Mino'
+            price='168$'
             />
             <Card
-            img='../Images/cardimageone.png' 
-            data='Zidan'
-            rating='5'
-            name='Pizda'
-            price='158$'
+            img={cardImage}
+            city='Germany'
+            rating='3'
+            name='Peter'
+            price='157$'
             />
             <Card
-            img='../Images/cardimageone.png' 
-            data='Zidan'
-            rating='5'
-            name='Pizda'
-            price='158$'
+            img={cardImage}
+            city='Italy'
+            rating='3'
+            name='John'
+            price='20$'
             />
         </div>
     )
