f7d27da8643fc420220e73ea345a990104c43b2f
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-04-07 15:06:03 +0200

    adding styles to card and contacts

---
+++ src/Components/Contact.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Card from '../Components/Card'
+
+export default function Contacts() {
+    return(
+        <div className='contacts'>
+            <Card
+            img='' 
+            data=''
+            rating=''
+            name=''
+            price=''
+            />
+            <Card
+            img='' 
+            data=''
+            rating=''
+            name=''
+            price=''
+            />
+            <Card
+            img='' 
+            data=''
+            rating=''
+            name=''
+            price=''
+            />
+            <Card
+            img='' 
+            data=''
+            rating=''
+            name=''
+            price=''
+            />
+        </div>
+    )
+}
